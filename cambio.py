9c57da8bd6b224d6b466f848f825d3ecb4412bce
Cancelado <rada.lmc@gmail.com>
2015-09-07 12:11:22 -0500

    Create cambio.py

---
+++ cambio.py
@@ -0,0 +1,13 @@
+def cambio(pesos):
+    """
+    >>> cambio(100)
+    6.0606060606060606
+    """
+    peso = pesos / 16.5
+    return peso
+    
+    return [porHombres, porMujeres]
+    
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
